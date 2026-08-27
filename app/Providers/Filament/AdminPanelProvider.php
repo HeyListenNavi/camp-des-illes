e34e8553e9a260127202a78c85f82d84cc253b56
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2026-08-27 02:18:04 -0700

    feat(filament): add custom filament theme to be able to use tailwind in custom components

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,13 +2,16 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Widgets\CampStatsOverview;
+use App\Filament\Widgets\LatestRegistrationsWidget;
+use App\Filament\Widgets\UpcomingEventsWidget;
+use App\Filament\Widgets\WelcomeBannerWidget;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
-use Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -25,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->viteTheme('resources/css/filament/admin/theme.css')
             ->login()
             ->colors([
                 'primary' => [
@@ -48,8 +52,10 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                WelcomeBannerWidget::class,
+                CampStatsOverview::class,
+                LatestRegistrationsWidget::class,
+                UpcomingEventsWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
